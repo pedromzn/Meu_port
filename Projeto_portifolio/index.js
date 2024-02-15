88235d30acfcd31cdeaeb5ea981185e7f1a6bfb5
Pedro Menzinger <105810911+pedromzn@users.noreply.github.com>
2024-02-15 16:07:48 -0300

    Add files via upload

---
+++ Projeto_portifolio/index.js
@@ -0,0 +1,63 @@
+// // funcionalidades de menu mobile
+// class MobileNavbar {
+//     constructor(mobileMenu, navList, navLinks) {
+//         this.mobileMenu = document.querySelector(mobileMenu);
+//         this.navList = document.querySelector(navList);
+//         this.navLinks = document.querySelectorAll(navLinks); 
+//         this.activeClass = 'active';
+
+//         this.handleClick = this.handleClick.bind(this); 
+//     } 
+
+//     handleClick() {
+//         this.navList.classList.toggle(this.activeClass); 
+//     }
+    
+
+//     addClickEvent() {
+//         this.mobileMenu.addEventListener('click', this.handleClick);
+//     }
+
+//     init() {
+//         if (this.mobileMenu) {
+//             this.addClickEvent();
+//         }
+//         return this;
+//     }
+// }
+
+// const mobileNavbar = new MobileNavbar(
+//     '.hamburger',
+//     '.nav-list',
+//     '.nav-list li'
+// );
+
+// mobileNavbar.init();
+
+const hamburger = document.querySelector('.hamburger')
+const nav = document.querySelector('.nav-list')
+
+hamburger.addEventListener('click', () =>{
+    nav.classList.toggle('active')
+})
+
+// alteração nos porjetos
+
+const btn_proje = document.querySelector('.btn-mostra')
+const projeto = document.querySelector('.projetos')
+
+btn_proje.addEventListener('click', () =>{
+    projeto.classList.toggle('active')
+})
+
+// dark and white mode
+let icon = document.getElementById('icon')
+
+icon.onclick = function(){
+    document.body.classList.toggle('white-theme')
+    if(document.body.classList.contains('white-theme')){
+        icon.src = 'imagens/moon.png'
+    }else{
+        icon.src = 'imagens/sun.png'
+    }
+}
